9dd86409988a16ec827bdc95a9790686d7cee2f8
John Hodmon <johnhodmon@gmail.com>
2016-02-07 18:58:21 +0000

    modified date attributes and radio button behaviour, job timings now being uploaded to server

--- app/src/main/java/ie/hodmon/computing/service_manager/controller/JobDetails.java
+++ app/src/main/java/ie/hodmon/computing/service_manager/controller/JobDetails.java
@@ -16,8 +16,11 @@ import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import ie.hodmon.computing.service_manager.R;
 import ie.hodmon.computing.service_manager.connection.ConnectionAPI;
@@ -56,10 +59,10 @@ public class JobDetails extends ClassForCommonAttributes implements RadioGroup.O
         jobDetailsPhone = (TextView) findViewById(R.id.job_details_phone_number);
         travelling = (RadioButton) findViewById(R.id.rb_travelling);
         jobProgress = (RadioGroup) findViewById(R.id.rg_job_progress);
-        jobProgress.setOnCheckedChangeListener(this);
+
         onSite = (RadioButton) findViewById(R.id.rb_on_site);
         beginJob = (RadioButton) findViewById(R.id.rb_start_job);
-        jobComplete = (RadioButton) findViewById(R.id.rb_start_job);
+        jobComplete = (RadioButton) findViewById(R.id.rb_job_complete);
         returnVisit = (RadioButton) findViewById(R.id.rb_return_visit);
         new GetJob(this).execute("/jobs/" + jobId);
 
@@ -149,6 +152,75 @@ public class JobDetails extends ClassForCommonAttributes implements RadioGroup.O
             jobDetailsPhone.setText(jobToDisplay.getCustomer().getPhone());
 
 
+            if(jobToDisplay.getStatus().equals("allocated"))
+            {
+
+                travelling.setEnabled(true);
+                onSite.setEnabled(false);
+                beginJob.setEnabled(false);
+                jobComplete.setEnabled(false);
+                returnVisit.setEnabled(false);
+
+            }
+
+            else if(jobToDisplay.getStatus().equals("travelling"))
+            {
+                travelling.setEnabled(false);
+                travelling.setChecked(true);
+                onSite.setEnabled(true);
+                beginJob.setEnabled(false);
+                jobComplete.setEnabled(false);
+                returnVisit.setEnabled(false);
+
+            }
+
+           else if(jobToDisplay.getStatus().equals("on site"))
+            {
+                travelling.setEnabled(false);
+                onSite.setEnabled(false);
+                onSite.setChecked(true);
+                beginJob.setEnabled(true);
+                jobComplete.setEnabled(false);
+                returnVisit.setEnabled(false);
+
+            }
+
+            else if(jobToDisplay.getStatus().equals("job started"))
+            {
+                travelling.setEnabled(false);
+                onSite.setEnabled(false);
+                beginJob.setEnabled(false);
+                beginJob.setChecked(true);
+                jobComplete.setEnabled(true);
+                returnVisit.setEnabled(true);
+
+            }
+
+            else if(jobToDisplay.getStatus().equals("complete"))
+            {
+                travelling.setEnabled(false);
+                onSite.setEnabled(false);
+                beginJob.setEnabled(false);
+                jobComplete.setEnabled(false);
+                returnVisit.setEnabled(false);
+                jobComplete.setChecked(true);
+
+            }
+
+            else if(jobToDisplay.getStatus().equals("return required"))
+            {
+                travelling.setEnabled(false);
+                onSite.setEnabled(false);
+                beginJob.setEnabled(false);
+                jobComplete.setEnabled(false);
+                returnVisit.setEnabled(false);
+                returnVisit.setChecked(true);
+
+            }
+
+
+            jobProgress.setOnCheckedChangeListener(JobDetails.this);
+
             if (dialog.isShowing()) {
                 dialog.dismiss();
             }
@@ -159,29 +231,60 @@ public class JobDetails extends ClassForCommonAttributes implements RadioGroup.O
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
 
+
+
         Log.v("radiobuttons", "Group: " + group);
         if (checkedId == travelling.getId()) {
-            jobToDisplay.setLabour_start(formatDate());
+            jobToDisplay.setTravel_start(formatDate());
+            jobToDisplay.setStatus("travelling");
 
-            onSite.setEnabled(true);
 
 
         }
 
-        new EditJob(this).execute("jobs/"+jobToDisplay.getId(),jobToDisplay);
+       else if (checkedId == onSite.getId()) {
+            jobToDisplay.setTravel_end("2016-02-07 20:00:00");
+            jobToDisplay.setStatus("on site");
+
+
+
+
+        }
+
+        else if (checkedId == beginJob.getId()) {
+            jobToDisplay.setLabour_start("2016-02-07 20:00:00");
+            jobToDisplay.setStatus("job started");
+          ;
+        }
+
+        else if (checkedId == jobComplete.getId())
+        {
+            jobToDisplay.setLabour_end("2016-02-07 21:00:00");
+            jobToDisplay.setStatus("complete");
+
+        }
+
+        else if (checkedId == returnVisit.getId())
+        {
+            jobToDisplay.setLabour_end(formatDate());
+            jobToDisplay.setStatus("return required");
+
+        }
+
+
+
+
+
+        new EditJob(this).execute("/jobs/"+jobToDisplay.getId(),jobToDisplay);
 
     }
 
     private String formatDate() {
-        Calendar now = Calendar.getInstance();
-        int year = now.get(Calendar.YEAR);
-        int month = now.get(Calendar.MONTH);
-        int day = now.get(Calendar.DAY_OF_MONTH);
-        int hour = now.get(Calendar.HOUR_OF_DAY);
-        int minute = now.get(Calendar.MINUTE);
-        int second = now.get(Calendar.SECOND);
-
-        return "Time.new(" + year + "," + month + "," + day + "," + hour + "," + minute + "," + second + "0)";
+        SimpleDateFormat dateFormat = new SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        Log.v("radiobuttons", "date being formatted " + dateFormat.format(date));
+        return dateFormat.format(date);
 
     }
 
